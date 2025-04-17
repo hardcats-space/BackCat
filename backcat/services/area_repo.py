c2c0deb130a27f85cd63d3d89bce0fdb83cc7720
Anton Kudryavtsev <dartt0n@ya.ru>
2025-04-17 12:42:07 +0300

    feat: area service

--- backcat/services/area_repo.py
+++ backcat/services/area_repo.py
@@ -1,4 +1,4 @@
-from typing import Protocol
+from typing import Protocol, TypedDict, Unpack
 
 from asyncpg import DataError, UniqueViolationError
 
@@ -6,17 +6,24 @@ from backcat import domain
 from backcat.database import tables
 from backcat.database.projector import ProjectionError, projection
 from backcat.services import errors
+from backcat.services.cache import Cache, Keyspace
+
+
+class UpdateArea(TypedDict):
+    polygon: list[domain.Point]
 
 
 class AreaRepo(Protocol):
     async def create_area(self, area: domain.Area, camping_id: domain.CampingID) -> domain.Area: ...
-    async def read_area(self, area_id: domain.AreaID) -> domain.Area: ...
-    async def update_area(self, area_id: domain.AreaID) -> domain.Area: ...
+    async def read_area(self, area_id: domain.AreaID) -> domain.Area | None: ...
+    async def update_area(self, area_id: domain.AreaID, **update: Unpack[UpdateArea]) -> domain.Area: ...
     async def delete_area(self, area_id: domain.AreaID) -> domain.Area: ...
 
 
 class AreaRepoImpl:
-    def __init__(self): ...
+    def __init__(self, cache: Cache):
+        self._ks = Keyspace("area")
+        self._cache = cache
 
     async def create_area(self, area: domain.Area, camping_id: domain.CampingID) -> domain.Area:
         try:
@@ -29,16 +36,16 @@ class AreaRepoImpl:
 
         try:
             async with tables.Area._meta.db.transaction():
-                db_area = (
-                    await tables.Area.insert(projection(area, camping_id=camping_id))
-                    .returning(*tables.Area.all_columns())
-                    .run()
-                )[0]
-                return projection(db_area, cast_to=domain.Area)
-        except UniqueViolationError as e:  # todo: is this the right exception?
+                db_area = (await tables.Area.insert(db_area).returning(*tables.Area.all_columns()).run())[0]
+                domain_area = projection(db_area, cast_to=domain.Area)
+
+                await self._cache.set(self._ks.key(domain_area.id.hex), domain_area, expire=self._cache.HOT_FEAT)
+
+                return domain_area
+        except UniqueViolationError as e:
             # unique violation error means that the area already exists
             raise errors.ConflictError("area already exists") from e
-        except DataError as e:  # todo: is this the right exception?
+        except DataError as e:
             # constraint violation error means that the area is invalid
             raise errors.ConflictError("invalid data object") from e
         except IndexError as e:
@@ -49,3 +56,61 @@ class AreaRepoImpl:
             raise errors.ConversionError("invalid area") from e
         except Exception as e:
             raise errors.InternalServerError("failed to insert area") from e
+
+    async def read_area(self, area_id: domain.AreaID) -> domain.Area | None:
+        try:
+            domain_area = await self._cache.get(self._ks.key(area_id.hex), t=domain.Area)
+            if domain_area is not None:
+                return domain_area
+
+            db_area = (
+                await tables.Area.objects()
+                .where(
+                    tables.Area.id == area_id,
+                    tables.Area.deleted_at.is_null(),
+                )
+                .first()
+                .run()
+            )
+            if db_area is None:
+                return None
+
+            return projection(db_area)
+        except ProjectionError as e:
+            # projection error means that the db data was read but it was not converted to domain model
+            raise errors.InternalServerError("failed to read area") from e
+        except Exception as e:
+            raise errors.InternalServerError("failed to read area") from e
+
+    async def update_area(self, area_id: domain.AreaID, **update: Unpack[UpdateArea]) -> domain.Area:
+        try:
+            await self._cache.invalidate(self._ks.key(area_id.hex))
+
+            values = {}
+            if "polygon" in update:
+                values[tables.Area.polygon] = update["polygon"]
+
+            async with tables.Area._meta.db.transaction():
+                db_area = (
+                    await tables.Area.update(values)
+                    .where(tables.Area.id == area_id)
+                    .returning(*tables.Area.all_columns())
+                    .run()
+                )[0]
+
+                domain_area = projection(db_area, cast_to=domain.Area)
+
+                await self._cache.set(self._ks.key(domain_area.id.hex), domain_area, expire=self._cache.HOT_FEAT)
+
+            return domain_area
+        except DataError as e:
+            # constraint violation error means that the area is invalid
+            raise errors.ConflictError("invalid data object") from e
+        except IndexError as e:
+            # index error means that no row was updated
+            raise errors.InternalServerError("update failed") from e
+        except ProjectionError as e:
+            # projection error means that the domain model was not converted to db data
+            raise errors.ValidationError("invalid area") from e
+        except Exception as e:
+            raise errors.InternalServerError("failed to update area") from e
--- backcat/services/cache.py
+++ backcat/services/cache.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import json
 from dataclasses import dataclass
-from typing import Any, overload
+from datetime import timedelta
+from typing import Any, TypeVar, overload
 
-import msgspec
+import pydantic
 from redis.asyncio import Redis
 
 
@@ -24,24 +26,54 @@ class Key:
         return f"{self.ks.name}:{':'.join(self.path)}"
 
 
+T = TypeVar("T", bound=pydantic.BaseModel)
+
+
 class Cache:
+    HOT_FEAT = timedelta(minutes=5)
+    COLD_FEAT = timedelta(minutes=30)
+
     def __init__(self, redis_dns):
         self._redis = Redis.from_url(redis_dns)
 
     @overload
-    async def get(self, key: Key) -> dict[str, Any] | None: ...
+    async def get(self, key: Key, *, silent: bool = True) -> dict[str, Any] | None: ...
 
     @overload
-    async def get(self, key: Key, t: type[msgspec.Struct]) -> msgspec.Struct | None: ...
+    async def get(self, key: Key, *, t: type[T], silent: bool = True) -> T | None: ...
 
-    async def get(self, key: Key, t: type[msgspec.Struct] | None = None) -> msgspec.Struct | dict[str, Any] | None:
-        value = await self._redis.get(key.as_str())
-        if value is None:
-            return None
-        return msgspec.json.decode(value, type=t)
+    async def get(self, key: Key, *, t: type[T] | None = None, silent: bool = True) -> T | dict[str, Any] | None:
+        try:
+            value = await self._redis.get(key.as_str())
+            if value is None:
+                return None
 
-    async def set(self, key: Key, value: msgspec.Struct | dict):
-        await self._redis.set(key.as_str(), msgspec.json.encode(value))
+            if t is None:
+                return json.loads(value)
+
+            return t.model_validate_json(value)
+        except Exception as e:
+            if not silent:
+                raise e
+
+    async def set(
+        self,
+        key: Key,
+        value: pydantic.BaseModel | dict,
+        *,
+        expire: timedelta | None = None,
+        silent: bool = True,
+    ):
+        try:
+            if isinstance(value, dict):
+                value_json = json.dumps(value)
+            else:
+                value_json = value.model_dump_json()
+
+            await self._redis.set(key.as_str(), value_json, ex=expire)
+        except Exception as e:
+            if not silent:
+                raise e
 
     async def invalidate(self, key: Key):
         await self._redis.delete(key.as_str())
