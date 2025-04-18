fe606768df130b91ac1f5729dd4ec4427b550527
Anton Kudryavtsev <dartt0n@ya.ru>
2025-04-18 19:57:29 +0300

    feat: add price to domain area

--- backcat/domain/area.py
+++ backcat/domain/area.py
@@ -1,9 +1,16 @@
-from pydantic import Field
+from pydantic import BaseModel, Field
+from pydantic_extra_types.currency_code import ISO4217
 
 from backcat.domain.base import DomainBaseModel
 from backcat.domain.id import AreaID
 from backcat.domain.point import Point
 
 
+class Price(BaseModel):
+    amount: int = Field(le=100)
+    currency: ISO4217
+
+
 class Area(DomainBaseModel[AreaID]):
     polygon: list[Point] = Field(min_length=3)
+    price: Price = Field()
