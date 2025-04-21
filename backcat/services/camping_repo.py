cbafdad2bab0e4778f0da6a2cda99b02f4ed6d78
Azamat Bayramov <bayramov.azamat04@gmail.com>
2025-04-21 16:24:52 +0300

    Create camping repo template

---
+++ backcat/services/camping_repo.py
@@ -0,0 +1,41 @@
+from typing import Protocol, TypedDict, Unpack
+
+from backcat import domain
+from backcat.domain import Point
+from backcat.services.cache import Cache, Keyspace
+
+
+class UpdateCamping(TypedDict):
+    polygon: list[Point]
+    title: str
+    description: str | None
+    thumbnails: list[str]
+
+
+class CampingRepo(Protocol):
+    async def create_camping(self, camping: domain.Camping, user_id: domain.UserID) -> domain.Camping: ...
+    async def read_camping(self, camping_id: domain.CampingID) -> domain.Camping | None: ...
+    async def read_campings(self, user_id: domain.UserID) -> list[domain.Camping]: ...
+    async def update_camping(self, camping_id: domain.CampingID, **update: Unpack[UpdateCamping]) -> domain.Camping: ...
+    async def delete_camping(self, camping_id: domain.CampingID) -> domain.Camping: ...
+
+
+class CampingRepoImpl:
+    def __init__(self, cache: Cache):
+        self._ks = Keyspace("camping")
+        self._cache = cache
+
+    async def create_camping(self, camping: domain.Camping, user_id: domain.UserID) -> domain.Camping:
+        pass
+
+    async def read_camping(self, camping_id: domain.CampingID) -> domain.Camping | None:
+        pass
+
+    async def read_campings(self, user_id: domain.UserID) -> list[domain.Camping]:
+        pass
+
+    async def update_camping(self, camping_id: domain.CampingID, **update: Unpack[UpdateCamping]) -> domain.Camping:
+        pass
+
+    async def delete_camping(self, camping_id: domain.CampingID) -> domain.Camping:
+        pass
