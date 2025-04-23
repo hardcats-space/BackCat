f4ce746293443726c75c887f03408f68881351e0
Azamat Bayramov <bayramov.azamat04@gmail.com>
2025-04-23 22:08:10 +0300

    Create POI ctrl

--- backcat/cmd/server/api/v1/poi/ctrl.py
+++ backcat/cmd/server/api/v1/poi/ctrl.py
@@ -1,7 +1,12 @@
-import litestar
-from litestar.dto import DTOData
+from typing import Annotated, Any
 
-from backcat import domain
+import litestar
+from dishka.integrations.litestar import FromDishka, inject
+from litestar.dto import DTOData
+from litestar.exceptions import NotFoundException
+from litestar.params import Parameter
+
+from backcat import domain, services
 from backcat.cmd.server.api.v1.poi import dto
 
 
@@ -10,21 +15,69 @@ class Controller(litestar.Controller):
     tags = ["poi"]
 
     @litestar.post("", dto=dto.CreatePOIRequest, return_dto=dto.CreatePOIResponse)
-    async def create(self, data: DTOData[domain.POI], camping_id: domain.CampingID) -> domain.POI:
-        raise NotImplementedError
+    @inject
+    async def create(
+        self,
+        camping_id: Annotated[domain.CampingID, Parameter(query="campingId")],
+        data: DTOData[domain.POI],
+        request: litestar.Request[domain.User, Any, Any],
+        poi_repo: FromDishka[services.POIRepo],
+    ) -> domain.POI:
+        return await poi_repo.create_poi(
+            request.user.id,
+            data.create_instance(**domain.POI.new_defaults_kwargs()),
+            camping_id,
+        )
 
     @litestar.get("/{id:uuid}", return_dto=dto.ReadPOIResponse)
-    async def read_one(self, id: domain.POIID) -> domain.POI:
-        raise NotImplementedError
+    @inject
+    async def read_one(
+        self,
+        id: domain.POIID,
+        request: litestar.Request[domain.User, Any, Any],
+        poi_repo: FromDishka[services.POIRepo],
+    ) -> domain.POI:
+        poi = await poi_repo.read_poi(request.user.id, id)
+        if poi is None:
+            raise NotFoundException(detail="poi not found")
+        return poi
 
     @litestar.get("", return_dto=dto.ReadManyPOIResponse)
-    async def read_many(self, camping_id: domain.CampingID) -> dto._ReadManyPOIs:
-        raise NotImplementedError
+    @inject
+    async def read_many(
+        self,
+        camping_id: Annotated[domain.CampingID, Parameter(query="campingId")],
+        request: litestar.Request[domain.User, Any, Any],
+        poi_repo: FromDishka[services.POIRepo],
+    ) -> dto._ReadManyPOIs:
+        return dto._ReadManyPOIs(
+            data=await poi_repo.filter_poi(
+                request.user.id,
+                services.poi_repo.FilterPOI(camping_id=camping_id),
+            )
+        )
 
     @litestar.patch("/{id:uuid}", dto=dto.UpdatePOIRequest, return_dto=dto.UpdatePOIResponse)
-    async def update(self, id: domain.POIID, data: DTOData[domain.POI]) -> domain.POI:
-        raise NotImplementedError
+    @inject
+    async def update(
+        self,
+        id: domain.POIID,
+        data: DTOData[domain.POI],
+        request: litestar.Request[domain.User, Any, Any],
+        poi_repo: FromDishka[services.POIRepo],
+    ) -> domain.POI:
+        return await poi_repo.update_poi(
+            request.user.id,
+            id,
+            services.poi_repo.UpdatePOI.model_validate(data.as_builtins()),
+        )
 
     @litestar.delete("/{id:uuid}")
-    async def delete(self, id: domain.POIID) -> None:
-        raise NotImplementedError
+    @inject
+    async def delete(
+        self,
+        id: domain.POIID,
+        request: litestar.Request[domain.User, Any, Any],
+        poi_repo: FromDishka[services.POIRepo],
+    ) -> None:
+        await poi_repo.delete_poi(request.user.id, id)
